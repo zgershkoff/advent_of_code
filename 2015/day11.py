7f345a38bb9eebc8af5fcb394f469a88a61a9c2a
Zach Gershkoff <zgershkoff@users.noreply.github.com>
2020-12-03 15:25:40 -0600

    2015 day 11 without programming

---
+++ 2015/day11.py
@@ -0,0 +1,6 @@
+"""
+This can be solved by inspection. It takes 5 characters to satisfy all
+the rules, in a configuration like 'aabcc'. My input was 'hepxcrrq', so
+leaving the x and everything to the left in place, the next password is
+'hepxxyzz'. Then the next one is 'heqaabcc'.
+"""
